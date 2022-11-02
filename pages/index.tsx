b7a6ec6af132d61bbe07102450eeaf71106f29db
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2022-11-03 08:06:38 +0900

    Feature/create footer components (#9)
    * 現在の年を取得
    
    * create Footer components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "../components/layouts/Footer";
 import { Header } from "../components/layouts/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
