45f5328b45920864a47d32fab92b3f6a002f488c
kca2250 <2250katok@gmail.com>
2022-11-06 23:29:35 +0900

    とりあえず記事一覧と詳細画面作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,39 @@
+import { NextPage } from "next";
+import Link from "next/link";
 import { Footer } from "../components/layouts/Footer";
 import { Header } from "../components/layouts/Header";
+import { client } from "../libs/client";
 
-export default function Home() {
+const Home: NextPage = ({ blogs }: any) => {
+  console.log(blogs);
   return (
     <>
       <Header />
+      <main>
+        <ul>
+          {blogs.map((blog: { id: string; title: string; content: string }) => (
+            <li key={blog.id}>
+              <Link href={`/${blog.id}`} passHref>
+                {blog.title}
+              </Link>
+              <span dangerouslySetInnerHTML={{ __html: `${blog.content}` }} />
+            </li>
+          ))}
+        </ul>
+      </main>
       <Footer />
     </>
   );
-}
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blogs" });
+  return {
+    props: {
+      blogs: data.contents,
+    },
+  };
+};
+
+export default Home;
