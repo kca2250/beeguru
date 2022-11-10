f9d3b2888f11f5c8b6f4f045a086eac1ef1bfbd9
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2022-11-10 23:05:31 +0900

    スタイルの調整と表示するコンテンツの追加 (#12)
    * ヘッダーコンポーネントの構成とスタイルの変更
    
    * 記事一覧のスタイルの変更と表示する情報の変更
    
    * カテゴリー一覧表示用コンポーネントの作成
    
    * Aboutコンポーネント作成
    
    * レイアウト変更とコンテンツ表示の追加

---
+++ components/About.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Profile from "../assets/about.jpg";
+
+export const About = () => {
+  return (
+    <div className="bg-slate-300 p-4 my-4">
+      <div className="flex gap-5 items-start">
+        <Image
+          className="rounded-lg"
+          width={50}
+          height={50}
+          src={Profile}
+          alt="about-me-img"
+        />
+        <div>
+          <h4 className="text-neutral-800 font-semibold">かと</h4>
+          <small>javascript / typescript / React / Next.js</small>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,66 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import { About } from "../components/About";
+import { Categories } from "../components/Categories";
 import { Footer } from "../components/layouts/Footer";
 import { Header } from "../components/layouts/Header";
 import { client } from "../libs/client";
 
-const Home: NextPage = ({ blogs }: any) => {
+const Home: NextPage = ({ blogs, categories }: any) => {
+  console.log(blogs);
+
   return (
-    <>
+    <div
+      className="mx-auto my-auto max-w-screen-md h-screen"
+      style={{ display: "grid", gridTemplateRows: "auto 1fr auto" }}
+    >
       <Header />
-      <main>
+
+      <main className="my-4 px-4">
         <ul>
-          {blogs.map((blog: { id: string; title: string; content: string }) => (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.id}`} passHref>
-                {blog.title}
-              </Link>
-              <span dangerouslySetInnerHTML={{ __html: `${blog.content}` }} />
-            </li>
-          ))}
+          {blogs.map((blog: any) => {
+            const year = new Date(blog.publishedAt).getFullYear();
+            const month = new Date(blog.publishedAt).getMonth();
+            const day = new Date(blog.publishedAt).getDay();
+            return (
+              <li className="py-3" key={blog.id}>
+                <Link href={`/${blog.id}`} passHref>
+                  <div className="flex items-center gap-3">
+                    <small className="bg-slate-600 text-white px-1 rounded">{`${year}年${month}月${day}日`}</small>
+                    <small className="text-slate-600">
+                      #{blog.category.name}
+                    </small>
+                  </div>
+                  <h2 className="py-1 text-cyan-700 hover:text-cyan-500  hover:decoration-cyan-500 hover:underline">
+                    {blog.title}
+                  </h2>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </main>
-      <Footer />
-    </>
+
+      <div>
+        <Categories categories={categories} />
+        <About />
+        <Footer />
+      </div>
+    </div>
   );
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blogs" });
+  const category = await client.get({ endpoint: "categories" });
+
+  console.log(category);
   return {
     props: {
       blogs: data.contents,
+      categories: category.contents,
     },
   };
 };
